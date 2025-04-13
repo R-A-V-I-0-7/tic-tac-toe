57bdee8ae6dd9c5e811ab36e37bb92db77379400
Ravi <xavier.110700@gmail.com>
2025-04-14 03:52:19 +0530

    Cleanup project: remove debugging files and simplify configuration

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/tic-tac-toe/',
+  server: {
+    port: 3000,
+    open: true
+  },
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
-  },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript'
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
+    emptyOutDir: true
   }
 }) 
